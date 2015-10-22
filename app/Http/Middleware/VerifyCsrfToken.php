30b81247df8722afe46e1b9e3a39bbf799408dcf
Frederick Lawler <fred@fredlawl.com>
2015-10-21 19:58:22 -0500

    setup csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Symfony\Component\Security\Core\Util\StringUtils;
 
 class VerifyCsrfToken extends BaseVerifier {
 
@@ -15,6 +16,25 @@ class VerifyCsrfToken extends BaseVerifier {
 	public function handle($request, Closure $next)
 	{
 		return parent::handle($request, $next);
+	}
+
+
+	/**
+	 * Determine if the session and input CSRF tokens match.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return bool
+	 */
+	protected function tokensMatch($request)
+	{
+		$token = $request->input('_token') ?: $request->session()->getToken();
+
+		if ( ! $token && $header = $request->header('X-XSRF-TOKEN'))
+		{
+			$token = $this->encrypter->decrypt($header);
+		}
+
+		return StringUtils::equals($request->session()->token(), $token);
 	}
 
 }
