1a5a2e6d77b2d15f4b1fd17a6045508045d023fe
Frederick Lawler <fred@fredlawl.com>
2015-11-18 21:45:18 -0600

    Added a bunch of models and repositories to collect data from DB

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -2,7 +2,9 @@
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\StatRepository;
 use App\Services\UserRepository;
+use App\Services\DataRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\MessageBag;
@@ -11,16 +13,51 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class DataController extends Controller {
 
 	private $userRepository;
+	private $dataRepository;
 
-	public function __construct (UserRepository $userRepository) {
+	public function __construct (UserRepository $userRepository, DataRepository $dataRepository) {
 		$this->userRepository = $userRepository;
+		$this->dataRepository = $dataRepository;
 	}
 
 
 	public function index()
 	{
-		// ... data
-		echo 'Logged in dude!';
+		$items = $this->dataRepository->fetchAll();
+		$table = [];
+
+		foreach ($items as $item) {
+			$user = $item->user()->first();
+
+			$user_info = array(
+				'name' => '',
+				'email' => '',
+				'occupation' => '',
+				'age' => '',
+				'gender' => ''
+			);
+
+			if (!empty($user)) {
+				$user_info['name'] = $user->name;
+				$user_info['email'] = $user->email;
+				$user_info['occupation'] = $user->occupation;
+				$user_info['age'] = $user->age;
+				$user_info['gender'] = $user->gender;
+			}
+
+			$table[] = array(
+				'session' => $item->session,
+				'category' => $item->category,
+				'difficulty' => $item->difficulty,
+
+				'city' => $item->city,
+				'state' => $item->state,
+				'zip' => $item->zip,
+				'country' => $item->country
+			) + $user_info;
+		}
+
+		return response()->json($table);
 	}
 
 
